e6b3e5f6d30e1b0ec49f5177cf8c342d8c46ef71
Ijaas Fouzer <96341377+IjaasMohamed@users.noreply.github.com>
2023-12-04 08:12:37 +0530

    edit function

--- Pages/Clients/Index.cshtml.cs
+++ Pages/Clients/Index.cshtml.cs
@@ -11,35 +11,34 @@ namespace MyStore.Pages.Clients
         {
             try
             {
-                String connectionString = "Data Source=.\\sqlexpress;Initial Catalog=mystore;Integrated Security=True;Trust Server Certificate=True";
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                String connectionString = "Data Source=.\\sqlexpress;Initial Catalog=mystore;Integrated Security=True";
+                using(SqlConnection connection = new SqlConnection(connectionString))
                 {
-                    connection.Open ();
-                    String sql = "SELECT id, name, email, phone, address, created_at AS created_at FROM clients\r\n";
-                    using (SqlCommand command = new SqlCommand (sql, connection))
+                    connection.Open();
+                    String sql = "SELECT * from clients";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
                     {
-                        using (SqlDataReader reader = command.ExecuteReader())
+                        using(SqlDataReader reader = command.ExecuteReader())
                         {
-                            while (reader.Read ())
+                            while (reader.Read())
                             {
-                                ClientInfo clientInfo = new ClientInfo ();
+                                ClientInfo clientInfo = new ClientInfo();
                                 clientInfo.id = "" + reader.GetInt32(0);
-                                clientInfo.name = reader.GetString (1);
-                                clientInfo.email = reader.GetString (2);
-                                clientInfo.phone = reader.GetString (3);
+                                clientInfo.name = reader.GetString(1);
+                                clientInfo.email = reader.GetString(2);
+                                clientInfo.phone = reader.GetString(3);
                                 clientInfo.address = reader.GetString(4);
-                                clientInfo.created_at = reader.GetString (5);
-
-                                Console.WriteLine($"ID: {clientInfo.id}, Name: {clientInfo.name}, Email: {clientInfo.email}, Phone: {clientInfo.phone}, Address: {clientInfo.address}, Created At: {clientInfo.created_at}");
+                                clientInfo.created_at = reader.GetString(5).ToString();
 
                                 listClients.Add(clientInfo);
                             }
                         }
                     }
                 }
-            } catch (Exception ex)
+
+            }catch (Exception ex)
             {
-                Console.WriteLine("Exception : " + ex.ToString());
+                Console.WriteLine("Exception: " + ex.Message);
             }
         }
     }
--- obj/Debug/net8.0/MyStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a10c401ec0e62647891bfe49e81d4cfc9a4d38b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5061723c8e31c36f0dfa8374d1c1799bfae9a7b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
